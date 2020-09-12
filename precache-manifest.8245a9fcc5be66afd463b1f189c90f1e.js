0b243842503009bfcc0b8cc40a523cf64f31c3ec
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-09-11 17:02:49 -0700

    Updates

---
+++ precache-manifest.8245a9fcc5be66afd463b1f189c90f1e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81d1615422b853ee08748c99f785d39",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "98a53b236cbf5a4e5471",
+    "url": "/ReactPortfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "16dfe35bc6fa0bacacea",
+    "url": "/ReactPortfolio/static/css/main.34be61b1.chunk.css"
+  },
+  {
+    "revision": "98a53b236cbf5a4e5471",
+    "url": "/ReactPortfolio/static/js/2.8e3c4a04.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactPortfolio/static/js/2.8e3c4a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16dfe35bc6fa0bacacea",
+    "url": "/ReactPortfolio/static/js/main.0d11f978.chunk.js"
+  },
+  {
+    "revision": "0e95178d86e25e0151b5",
+    "url": "/ReactPortfolio/static/js/runtime-main.f3c64b5a.js"
+  },
+  {
+    "revision": "e0ad72cd777dc418117b18c7f98d499e",
+    "url": "/ReactPortfolio/static/media/KNgo_DES325_Poster.e0ad72cd.png"
+  },
+  {
+    "revision": "02b6f9669acb55d46eddbb5f1bbad07b",
+    "url": "/ReactPortfolio/static/media/Mumble.02b6f966.png"
+  },
+  {
+    "revision": "8997cf27a8fc64945f7a578373edf304",
+    "url": "/ReactPortfolio/static/media/Screen Shot 2020-09-04 at 2.53.35 PM.8997cf27.png"
+  },
+  {
+    "revision": "9833372e715eba712b420f16e1987d56",
+    "url": "/ReactPortfolio/static/media/Screen Shot 2020-09-04 at 4.22.50 PM.9833372e.png"
+  },
+  {
+    "revision": "001a0232472f2c52a460ca8a1d7fca8a",
+    "url": "/ReactPortfolio/static/media/Screen Shot 2020-09-11 at 12.53.30 PM.001a0232.png"
+  },
+  {
+    "revision": "a66f92268b4b644fa1f5c0243b798c64",
+    "url": "/ReactPortfolio/static/media/Screen Shot 2020-09-11 at 3.19.03 PM.a66f9226.png"
+  },
+  {
+    "revision": "d8112681f78ce3742319bbbde0cf742f",
+    "url": "/ReactPortfolio/static/media/kenny_ngo_headshot.d8112681.jpg"
+  }
+]);
